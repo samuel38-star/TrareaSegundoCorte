fc0baa05d6e518baee260975862eb5639a7a2159
Samuel R <samuel384695@gmail.com>
2026-05-27 00:12:24 -0500

    first commit

--- models.py
+++ models.py
@@ -2,7 +2,12 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 
 class Paciente(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
+
     nombre: str
     enfermedad: str
     prioridad: int
+
+    en_cola: bool = False
+
+    atendido: bool = False
